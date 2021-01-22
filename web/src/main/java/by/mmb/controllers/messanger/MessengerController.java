66305908e0291e031f092a023a233f0aac72612d
Maksimovich1 <andrey.maksimovich.96@mail.ru>
2021-01-22 11:03:39 +0300

    Пока понял что нужно сделать заявки

---
+++ web/src/main/java/by/mmb/controllers/messanger/MessengerController.java
@@ -0,0 +1,15 @@
+package by.mmb.controllers.messanger;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Конроллер для реализации месенджера
+ * TODO web socket or async
+ *
+ * @author andrew.maksimovich
+ */
+@RestController
+@RequestMapping("api/mess")
+public class MessengerController {
+}
